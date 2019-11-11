fe7ec090dd910c803b80ae6540cc9d711a7f0d84
aaltopiiri <aaltopiiri@outlook.com>
2019-11-11 13:12:18 +0200

    Add nginx.conf

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM nginx
 RUN rm /etc/nginx/conf.d/default.conf
 #COPY content /usr/share/nginx/html
 EXPOSE 80
-ADD conf /etc/nginx/conf.d
+COPY conf /etc/nginx/conf.d
+
 #WORKDIR /usr/share/nginx/html
 #VOLUME [ "./content:/usr/share/nginx/html" ]
--- content/js/script.js
+++ content/js/script.js
@@ -1,10 +1,8 @@
 let d = new Date();
 var currentHours = d.getHours ( );
-var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
 var currentMinutes = d.getMinutes ( );
 var currentSeconds = d.getSeconds ( );
-currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+currentHours = ( currentHours < 10 ? "0" : "") + currentHours;
 currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
 currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
-document.body.innerHTML = "Time right now is:  " + currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay
+document.body.innerHTML = "Time right now is:  " + currentHours + ":" + currentMinutes + ":" + currentSeconds;
