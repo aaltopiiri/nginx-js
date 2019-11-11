a398786ca097ad81bb975c1c97a3227afc1fa989
aaltopiiri <aaltopiiri@outlook.com>
2019-11-11 13:43:23 +0200

    Create JS realtime clock

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
 FROM nginx
 RUN rm /etc/nginx/conf.d/default.conf
-#COPY content /usr/share/nginx/html
-EXPOSE 80
+COPY content /usr/share/nginx/html
 COPY conf /etc/nginx/conf.d
 
-#WORKDIR /usr/share/nginx/html
-#VOLUME [ "./content:/usr/share/nginx/html" ]
+EXPOSE 80
--- content/js/script.js
+++
@@ -1,8 +0,0 @@
-let d = new Date();
-var currentHours = d.getHours ( );
-var currentMinutes = d.getMinutes ( );
-var currentSeconds = d.getSeconds ( );
-currentHours = ( currentHours < 10 ? "0" : "") + currentHours;
-currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
-document.body.innerHTML = "Time right now is:  " + currentHours + ":" + currentMinutes + ":" + currentSeconds;
